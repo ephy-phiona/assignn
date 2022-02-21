1745638609912ae840a6e8e899dafe811d5badc3
ephy-phiona <ephyphiona@gmail.com>
2022-02-21 08:33:58 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    helloname()
+    remainder()
+    sum()
+    fact()
+}fun helloname(){
+    var name="Phiona"
+    println("Hello "+name)
+}
+fun remainder(){
+    var a = 11
+    var b = 4
+    var modulus = a%b
+    println(modulus)
+}
+fun sum(){
+    var a = 4
+    var b = 3
+    var c = 7
+    var d = 5
+    var sum = a + b + c + d
+    println(sum)
+}
+fun fact(){
+   println("I am always happy")}
