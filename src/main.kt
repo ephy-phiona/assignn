27fe214d79b69eef2f7ff3915bb290558aebc84b
ephy-phiona <ephyphiona@gmail.com>
2022-02-23 19:08:29 +0300

    first commit

--- src/main.kt
+++ src/main.kt
@@ -1,25 +1,32 @@
 fun main(){
-    helloname()
-    remainder()
-    sum()
-    fact()
-}fun helloname(){
-    var name="Phiona"
-    println("Hello "+name)
+    var name=helloname("hello Phiona")
+    var hello=helloname("hello tamara")
+println(name)
+    println(hello)
+    var modulus=remainder(70,4)
+    var mode=remainder(30, 40)
+     println(modulus)
+    println(mode)
+    var y=sum(10, 20, 30, 40)
+    var x=sum(20, 40, 60, 80)
+    println(y)
+     println(x)
+var g = fact( "party","hollic")
+    println(g)
+}fun helloname(name: String):String{
+    var name=("hello $name")
+    return name
 }
-fun remainder(){
-    var a = 11
-    var b = 4
-    var modulus = a%b
-    println(modulus)
+fun remainder(a: Int,b: Int):Int{
+    var remainder=a%b
+    return remainder
+
 }
-fun sum(){
-    var a = 4
-    var b = 3
-    var c = 7
-    var d = 5
+fun sum(a: Int,b: Int,c: Int,d: Int):Int{
     var sum = a + b + c + d
-    println(sum)
+    return sum
 }
-fun fact(){
-   println("I am always happy")}
+fun fact(name1: String,name2: String):String{
+    var fact=("$name1, $name2")
+    return fact
+}
